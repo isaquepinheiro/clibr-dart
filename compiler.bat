a5aa9229b17a7e5f0e638f00aa791126de889a79
IsaquePinheiro <isaquesp@gmail.com>
2023-07-12 07:23:49 -0300

    build

--- compiler.bat
+++ compiler.bat
@@ -3,6 +3,6 @@ rem dart compile exe bin/dmfbr.dart -o bin/dmfbr_win.exe --target-os windows
 rem dart compile exe bin/dmfbr.dart -o bin/dmfbr_linux --target-os linux
 rem dart compile exe bin/dmfbr.dart -o bin/dmfbr_macos --target-os macos
 
-7z a -tzip bin/dmfbr_windows-x64.zip bin/dmfbr.exe bin/\*.pas -r
-rem 7z a -tzip bin/dmfbr_linux-amd64.zip bin/dmfbr_linux bin/\*.pas -r
-rem 7z a -tzip bin/dmfbr_macos-x64.zip bin/dmfbr_macos bin/\*.pas -r
+7z a -tzip bin/dmfbr_windows-x64.zip bin/dmfbr.exe bin/templates/* -r
+rem 7z a -tzip bin/dmfbr_linux-amd64.zip bin/dmfbr_linux bin/templates/*.pas -r
+rem 7z a -tzip bin/dmfbr_macos-x64.zip bin/dmfbr_macos bin/templates/*.pas -r
